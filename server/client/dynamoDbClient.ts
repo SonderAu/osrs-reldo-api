8643a28c75ae5501f0fa871b5e7db4cca8e1dd3b
sonderau <bailey@yeahnah.net>
2024-11-14 20:26:09 +0800

    Begun building out framework for storing hiscores and userdata in db, reinstate dynamoDBClient.ts as fails to build when deleted. Frontend not yet ready for these changes

--- server/client/dynamoDbClient.ts
+++ server/client/dynamoDbClient.ts
@@ -1,8 +1,6 @@
 import AWS from 'aws-sdk';
 import dotenv from 'dotenv';
 
-const DEFAULT_RSN = 'default';
-
 export class DynamoDbClient {
   private readonly client;
 
@@ -18,155 +16,50 @@ export class DynamoDbClient {
     this.client = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
   }
 
-  createUser(
-    email: string,
-    rsn: string | undefined,
-    responseHandler: (response: AWS.DynamoDB.GetItemOutput | null) => void,
-  ): void {
+  createUser(email: string, rsn: string): void {
     const params = {
       TableName: process.env.DDB_USERS_TABLE_NAME ?? '',
       Item: {
         email: { S: email },
-        rsn: { S: rsn ?? DEFAULT_RSN },
+        rsn: { S: rsn },
       },
     };
-
     this.client.putItem(
       params,
       (err: AWS.AWSError | null, data: AWS.DynamoDB.PutItemOutput | null) => {
         if (err !== null) {
           console.log('Error', err);
-          throw new Error(err.message);
         } else {
           console.log('Success', data);
-          responseHandler(data);
         }
       },
     );
   }
 
-  getUser(
-    email: string | undefined,
-    rsn: string | undefined,
-    responseHandler: (response: AWS.DynamoDB.GetItemOutput | null) => void,
-  ): void {
-    const params: any = {
+  getUser(email: string, rsn: string): AWS.DynamoDB.PutItemOutput | null {
+    const params = {
       Key: {
+        email: {
+          S: email,
+        },
         rsn: {
-          S: rsn ?? DEFAULT_RSN,
+          S: rsn,
         },
       },
       TableName: process.env.DDB_USERS_TABLE_NAME ?? '',
     };
-
-    if (email) {
-      params.Key.email = {
-        S: email,
-      };
-    }
-
+    let result = null;
     this.client.getItem(
       params,
-      (err: AWS.AWSError | null, data: AWS.DynamoDB.GetItemOutput | null) => {
-        if (err !== null) {
-          console.log('Error', err);
-          throw new Error(err.message);
-        } else {
-          console.log('Success', data);
-          responseHandler(data);
-        }
-      },
-    );
-  }
-
-  getUserByRsn(
-    rsn: string,
-    responseHandler: (response: AWS.DynamoDB.ScanOutput | null) => void,
-  ): void {
-    const params: any = {
-      FilterExpression: `attribute_exists(tasks_${rsn})`,
-      TableName: process.env.DDB_USERS_TABLE_NAME ?? '',
-    };
-
-    this.client.scan(
-      params,
-      (err: AWS.AWSError | null, data: AWS.DynamoDB.GetItemOutput | null) => {
-        if (err !== null) {
-          console.log('Error', err);
-          throw new Error(err.message);
-        } else {
-          console.log('Success', data);
-          responseHandler(data);
-        }
-      },
-    );
-  }
-
-  putUser(
-    email: string,
-    rsn: string | undefined,
-    key: string,
-    payload: string,
-    responseHandler: (response: AWS.DynamoDB.GetItemOutput | null) => void,
-  ): void {
-    const params = {
-      TableName: process.env.DDB_USERS_TABLE_NAME ?? '',
-      Item: {
-        email: { S: email },
-        rsn: { S: rsn ?? DEFAULT_RSN },
-        [key]: { S: JSON.stringify(payload) },
-      },
-    };
-
-    this.client.putItem(
-      params,
       (err: AWS.AWSError | null, data: AWS.DynamoDB.PutItemOutput | null) => {
         if (err !== null) {
           console.log('Error', err);
-          throw new Error(err.message);
         } else {
           console.log('Success', data);
-          responseHandler(data);
+          result = data;
         }
       },
     );
-  }
-
-  updateUser(
-    email: string,
-    rsn: string | undefined,
-    key: string,
-    payload: string,
-    responseHandler: (response: AWS.DynamoDB.GetItemOutput | null) => void,
-  ): void {
-    const params = {
-      ExpressionAttributeNames: {
-        '#Key': key,
-      },
-      ExpressionAttributeValues: {
-        ':value': {
-          S: JSON.stringify(payload),
-        },
-      },
-      Key: {
-        email: { S: email },
-        rsn: { S: rsn ?? DEFAULT_RSN },
-      },
-      ReturnValues: 'ALL_NEW',
-      TableName: process.env.DDB_USERS_TABLE_NAME ?? '',
-      UpdateExpression: 'SET #Key = :value',
-    };
-    this.client.updateItem(
-      params,
-      (err: AWS.AWSError | null, data: AWS.DynamoDB.PutItemOutput | null) => {
-        if (err !== null) {
-          console.log('Error', err);
-          throw new Error(err.message);
-        } else {
-          console.log('Success', data);
-          responseHandler(data);
-        }
-      },
-    );
+    return result;
   }
 }
