722cf828c11866f7dd12241ab830a7455176fd17
sonderau <bailey@yeahnah.net>
2024-11-24 19:42:39 +0800

    Initial commit. Updated all packages. Removed deprecated ones in lieu of like for like replacements. Removed references to dynamoDBClient. Moved to Prettier alongside ESLint as previous copies were deprecated also. Updated Node version and Typescript versions. Builds and runs just fine, no other changes made yet.

--- server/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-/* GET home page. TODO make this display swagger UI */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Reldo API' });
-});
-
-export default router;
